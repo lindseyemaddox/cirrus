3bdbb8cffeb27f4c6fb93fe035d4a171ace45c39
lindseyemaddox <lindseyemaddox@gmail.com>
2014-10-26 20:36:34 -0500

    why didn't you commit?

---
+++ L2501HST.php
@@ -0,0 +1,131 @@
+<!doctype html>
+
+<!--[if IE 9 ]><html lang="en" class="ie9"><![endif]-->
+<!--[if lt IE 9 ]><html lang="en" class="ie8"><![endif]-->
+<!--[if lt IE 8 ]><html lang="en" class="ie7"><![endif]-->
+<!--[if (gt IE 7)|!(IE)]><!--><html lang="en" class="no-js"><!--<![endif]-->
+
+<head>
+	<meta charset="utf-8">
+	
+	<!-- Stylesheets -->
+	<link rel="stylesheet" href="/_styles/css/common.css" media="screen">
+
+</head>
+
+<div class="info">
+
+	<div class="header">
+		
+		<h1><img src="/_images/logo-kubota-grey.png" alt="Kubota logo grey"></h1>
+
+		<h2>L2501HST Package Deal</h2>
+
+		<div class="clear"></div>
+
+	</div><!--header-->
+	
+	<div class="left">
+		
+		<img src="/_images/Kubota-L2501-Tractor-Package.jpg">
+
+		<h2>KUBOTA L2501HST</h2>
+
+		<h3>Engine</h3>
+
+		<ul>
+			<li>Engine gross horsepower<span>24.8 (18.5) HP (kW)</span></li>
+			<li>Engine net power<span>23.9 (17.8) HP (kW)</span></li>
+			<li>PTO power<span>19.0 (14.2) HP (kW)</span></li>
+			<li>No. of cylinders<span>3</span></li>
+			<li>Bore & stroke<span>3.4 × 3.6 (87 × 92.4) HP (kW)</span></li>
+			<li>Total displacement<span>100.47 (1647) cu. in. (cm3)</span></li>
+			<li>Rated speed<span>2200 rpm</span></li>
+			<li>Battery<span>12V, RC: 123 min, CCA: 490 A</span></li>
+			<li>Fuel tank capacity<span>10.0 US gal.</span></li>
+		</ul>
+
+		<h3>PTO</h3>
+
+		<ul>
+			<li>Rear PTO type<span>Live-continuous running</span></li>
+			<li>PTO/Engine speedsystem<span>540/2105</span></li>
+		</ul>
+
+		<h3>Hydraulic</h3>
+
+		<ul>
+			<li>Pump capacity (main)<span>5.15 gpm</span></li>
+			<li>Pump capacity (PS)<span>3.13 gpm</span></li>
+			<li>3-point hitch</li>
+			<li>Lift capacity at lift points<span>1918 (870) lbs. (kg)</span></li>
+			<li>Lift capacity at 24 in. behind lift points<span>1389 (630) lbs. (kg)</span></li>
+			<li>System pressure<span>2205 (15.2) kgf/cm2 (MPa)</span></li>
+		</ul>
+
+		<h3>Traveling System</h3>
+
+		<ul>
+			<li>Transmission<span>Hydrostatic transmission, 3 range speed</span></li>
+			<li>Steering<span>Integral type power steering</span></li>
+			<li>Brake<span>Wet disc type</span></li>
+			<li>Clutch<span>Dry type single stage</span></li>
+		</ul>
+
+		<h3>Tire size</h3>
+
+		<ul>
+			<li>AG Standard<span>7.2 - 16 / 11.2 - 24</span></li>
+			<li>TURF Option<span>N/A</span></li>
+			<li>Indust. Option<span>27 × 8.50 - 15/ 15 - 19.5 R4</span></li>
+		</ul>
+
+		<h3>Traveling speed</h3>
+
+		<ul>
+			<li>with standard AG tires<span>Forward 0 (0.0) - 11.5 (18.5), Reverse 0 (0.0) - 10.3 (16.6) mph (km/h)</span></li>
+		</ul>
+
+		<h3>Dimensions</h3>
+
+		<ul>
+			<li>Overall length (w/o 3P)<span>106.3 (2700) in. (mm)</span></li>
+			<li>Overall width (min.tread)<span>55.1 (1400) in. (mm)</span></li>
+			<li>Wheelbase<span>63.3 (1610) in. (mm)</span></li>
+			<li>Min. ground clearance<span>13.4 (340) in. (mm)</span></li>
+			<li>Rear tire tread<span>43.1 (1095) in. (mm)</span></li>
+			<li>Front tire tread<span>43.8 (1115) / 47.1 (1195) / 50.8 (1290) in. (mm)</span></li>
+			<li>Min. turning radius (w/brake, 4WD disengaged)<span>8.2 (2.5) feet (m)</span>
+			<li>Weight (with ROPS)<span>2623 (1190) lbs. (kg)</span></li>
+		</ul>
+		
+	</div><!--left-->
+
+	<div class="right">
+			
+		<img src="/_images/5ft-rotary-cutter.jpg">
+
+		<h2>5FT Rotary Cutter</h2>
+
+		<ul>
+			<li>Gearbox rating: 40HP</li>
+			<li>Cutting height: 2"-10"</li>
+			<li>Deck Material: 12 gauge</li>
+			<li>Approximate weight: 557lbs</li>
+		</ul>
+
+		<h2>20FT Utility Trailer</h2>
+
+		<ul>
+			<li>Two 3500lb axles</li>
+			<li>Slide in ramps</li>
+			<li>New Tires</li>
+			<li>Set back jack</li>
+			<li>Trailer brakes</li>
+		</ul>
+
+	</div><!--right-->
+
+	<div class="clear"></div>
+
+</div><!--info-->
---
+++ L3301HST.php
@@ -0,0 +1,140 @@
+<!doctype html>
+
+<!--[if IE 9 ]><html lang="en" class="ie9"><![endif]-->
+<!--[if lt IE 9 ]><html lang="en" class="ie8"><![endif]-->
+<!--[if lt IE 8 ]><html lang="en" class="ie7"><![endif]-->
+<!--[if (gt IE 7)|!(IE)]><!--><html lang="en" class="no-js"><!--<![endif]-->
+
+<head>
+	<meta charset="utf-8">
+	
+	<!-- Stylesheets -->
+	<link rel="stylesheet" href="/_styles/css/common.css" media="screen">
+
+</head>
+
+<div class="info">
+
+	<div class="header">
+		
+		<h1><img src="/_images/logo-kubota-grey.png" alt="Kubota logo grey"></h1>
+
+		<h2>L3301HST Package Deal</h2>
+
+		<div class="clear"></div>
+
+	</div><!--header-->
+	
+	<div class="left">
+		
+		<img src="/_images/Kubota-L3301-Tractor-Package.jpg">
+
+		<h2>KUBOTA L3301HST</h2>
+
+		<h3>Engine</h3>
+
+		<ul>
+			<li>Type<span>Direct injection, vertical, water-cooled 4-cycle diesel</span></li>
+			<li>Engine gross power<span>33.0 (24.6) HP (kW)</span></li>
+			<li>Engine net power<span>31.4 (23.4) HP (kW)</span></li>
+			<li>PTO power<span>26.2 (19.5) HP (kW)</span></li>
+			<li>No. of cylinders<span>3</span></li>
+			<li>Bore & stroke<span>3.4 × 4.0 (87 × 102.4) in. (mm)</span></li>
+			<li>Total displacement<span>111.4 (1826)</span></li>
+			<li>Rated speed<span>2700 rpm</span></li>
+			<li>Battery<span>12V, RC: 110 min, CCA: 580 A</span></li>
+			<li>Alternator<span>12V, 40A</span></li>
+			<li>Fuel tank capacity<span>11.1 (42) US gal.</span></li>
+		</ul>
+
+		<h3>PTO</h3>
+
+		<ul>
+			<li>Rear PTO type<span>Live - with overrunning clutch</span></li>
+			<li>PTO/Engine speed<span>540/2580</span></li>
+		</ul>
+
+		<h3>Hydraulic</h3>
+
+		<ul>
+			<li>Lift control type<span>Position control</span></li>
+			<li>Pump capacity (main)<span>6.3 (23.9) gpm</span></li>
+			<li>Pump capacity (ps)<span>3.8 (14.5) gpm</span></li>
+			<li>3-point hitch<span>Category I</span></li>
+			<li>Lift capacity at lift points<span>1985 (900) lbs. (kg)</span></li>
+			<li>Lift capacity 24 in. behind lift points<span>1433 (650) lbs. (kg)</span></li>
+			<li>System pressure<span>165 (16.2) kgf/cm2 (MPa)</span></li>
+		</ul>
+
+		<h3>Traveling System</h3>
+
+		<ul>
+			<li>Transmission<span>Hydrostatic transmission, 3 range speed</span></li>
+			<li>Steering<span>Integral type power steering</span></li>
+			<li>Brake<span>Mechanical, wet disc type</span></li>
+			<li>Clutch<span>Dry type single stage</span></li>
+		</ul>
+
+		<h3>Tire size</h3>
+
+		<ul>
+			<li>AG Standard<span>7.2 - 16 /11.2 - 24 Front/Rear</span></li>
+			<li>TURF Option<span>25 × 8.50-14/13.6-16 Front/Rear</span></li>
+			<li>Indust. Option<span>27 × 8.50 - 15/15 - 19.5 R4</span></li>
+		</ul>
+
+		<h3>Traveling speed</h3>
+
+		<ul>
+			<li>with standard AG tires<span>Forward 0.0 (0.0) - 14.1 (22.7), Reverse 0.0 (0.0) - 12.7 (20.5)</span></li>
+		</ul>
+
+		<h3>Dimensions</h3>
+
+		<ul>
+			<li>Overall length (w/o 3P)<span>107.9 (2740) in. (mm)</span></li>
+			<li>Overall width (min. tread)<span>55.1 (1400) in. (mm)</span></li>
+			<li>Overall height (w/Foldable ROPS)<span>91.7 (2330) in. (mm)</span></li>
+			<li>Wheelbase<span>63.3 (1610) in. (mm)</span></li>
+			<li>Min. ground clearance<span>13.4 (340) in. (mm)</span></li>
+			<li>Front tire tread<span>43.1 (1095) in. (mm)</span></li>
+			<li>Rear tire tread<span>43.8 (1115) / 47.1 (1195) / 50.8 (1290) in. (mm)</span></li>
+			<li>Min. turning radius (w/brake, 4WD disengaged)<span>8.2 (2.5) feet (m)</span></li>
+		</ul>
+
+		<h3>Weight</h3>
+
+		<ul>
+			<li>With ROPS<span>2778 (1260) lbs. (kg)</span></li>
+		</ul>
+
+	</div><!--left-->
+
+	<div class="right">
+			
+		<img src="/_images/5ft-rotary-cutter.jpg">
+
+		<h2>5FT Rotary Cutter</h2>
+
+		<ul>
+			<li>Gearbox rating: 40HP</li>
+			<li>Cutting height: 2"-10"</li>
+			<li>Deck Material: 12 guage</li>
+			<li>Approximate weight: 557lbs</li>
+		</ul>
+
+		<h2>20FT Utility Trailer</h2>
+
+		<ul>
+			<li>Two 3500lb axles</li>
+			<li>Slide in ramps</li>
+			<li>New Tires</li>
+			<li>Set back jack</li>
+			<li>Trailer brakes</li>
+		</ul>
+
+	</div><!--right-->
+
+	<div class="clear"></div>
+
+</div><!--info-->
