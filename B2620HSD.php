1666eb5a3ffc8f51c3c82db3f2c7280fe02c2cc8
Lindsey Maddox <lindseyemaddox@gmail.com>
2014-12-01 09:45:17 -0600

    updated specs...

---
+++ B2620HSD.php
@@ -0,0 +1,157 @@
+<!doctype html>
+
+<!--[if IE 9 ]><html lang="en" class="ie9"><![endif]-->
+<!--[if lt IE 9 ]><html lang="en" class="ie8"><![endif]-->
+<!--[if lt IE 8 ]><html lang="en" class="ie7"><![endif]-->
+<!--[if (gt IE 7)|!(IE)]><!--><html lang="en" class="no-js"><!--<![endif]-->
+
+<head>
+	<meta charset="utf-8">
+	
+	<!-- Stylesheets -->
+	<link rel="stylesheet" href="/_styles/css/common.css" media="screen">
+
+</head>
+
+<div class="info">
+
+	<div class="header">
+		
+		<h1><img src="/_images/logo-kubota-grey.png" alt="Kubota logo grey"></h1>
+
+		<h2>B2620HSD Package Deal</h2>
+
+		<div class="clear"></div>
+
+	</div><!--header-->
+	
+	<div class="left">
+		
+		<img src="/_images/Kubota-B2620HSD-Tractor-Package.jpg">
+
+		<h3>Engine</h3>
+
+		<ul>
+			<li>Type<span>E-TVCS, liquid cooled, 3-cylinder diesel</span></li>
+			<li>Make, Model<span>Kubota D1105</span></li>
+			<li>PTO horsepower<span>DT:19.0HP, 14.2kW</span></li>
+			<li>Engine gross horsepower<span>26.0HP, 19.4kW</span></li>
+			<li>Rated RPM<span>2800rpm</span></li>
+			<li>Bore and stroke<span>3.1 x 3.1in, 78 x 78.4mm</span></li>
+			<li>Displacement<span>68.5cu.in., 1123cc</span></li>
+			<li>Air cleaner<span>Dual paper element</span></li>
+			<li>Lubrication<span>Forced lubrication</span></li>
+			<li>Cooling system<span>Pressurized radiator</span></li>
+			<li>Radiator fan<span>Radial air flow</span></li>
+		</ul>
+
+		<h3>Capacities</h3>
+
+		<ul>
+			<li>Fuel tank<span>6.9gal, 26l</li>
+			<li>Cooling system<span>4.1qts, 3.9l</span></li>
+			<li>Engine oil<span>3.3qts, 3.1l</span></li>
+			<li>Transmission case<span>4.0gal, 15l</span></li>
+		</ul>
+
+		<h3>Drive train</h3>
+
+		<ul>
+			<li>Clutch<span>Dry single plate</span></li>
+			<li>Transmission<span>HST (3 range)</span></li>
+			<li>differential lock<span>Standard</span></li>
+			<li>Final drive<span>Spur gear</span></li>
+			<li>Brake<span>Wet disc type</span></li>
+			<li>PTO<span>Transmission driven with one way clutch, Rear: 540,960rpm, Mid: 2500rpm</span></li>
+		</ul>
+
+		<h3>Hydraulics</h3>
+
+		<ul>
+			<li>Type<span>Open Center, Gear Pump</span></li>
+			<li>Pump<span>Dual Pump, 3P: 4.7 (17.9) Power Steering: 3.6 (13.5)</span></li>
+			<li>Total pump output<span>8.3gpm, 31.4l/min</span></li>
+			<li>Hydraulic outlet<span>Front outlet (w/standard valve)</span></li>
+			<li>3-point hitch<span>Category 1</span></li>
+			<li>Hydraulic control system<span>Quarter inching valve</span></li>
+			<li>Lift capacity<span>at lift point: 1356lbs, 615kg, 24" behind lift point: 1058lbs, 480kg</span></li>
+			<li>Steering<span>Integral type power steering</span></li>
+		</ul>
+
+		<h3>Standard tire size</h3>
+
+		<ul>
+			<li>Front<span>7-12</span></li>
+			<li>Rear<span>11.2-16</span></li>
+		</ul>
+
+		<h3>Traveling speeds</h3>
+
+		<ul>
+			<li>Forward<span>0-12.6mph, 0-20.2km/h</span></li>
+			<li>Reverse<span>0-10.0mph, 0-16.1km/h</span></li>
+		</ul>
+
+		<h3>Dimensions</h3>
+
+		<ul>
+			<li>Overall length<span>(without 3-point hitch) 95.1in, 2415mm</span></li>
+			<li>Overall width<span>(min tread) 49in, 1245mm</span></li>
+			<li>Wheelbase<span>61.5in, 1563mm</span></li>
+			<li>Ground clearance<span>12.8in, 325mm</span></li>
+			<li>Tread<span>Front: 32.1in, 815mm, Rear: 37.4in, 950mm</span></li>
+			<li>Turning radius with brake<span>6.9feet, 2.1m</span></li>
+			<li>Weight<span>1554lbs, 705kg</span></li>
+		</ul>
+
+	</div><!--left-->
+
+	<div class="right">
+			
+		<img src="/_images/4ft-rotary-cutter.jpg">
+
+		<h2>4FT Rotary Cutter</h2>
+
+		<ul>
+			<li>Gearbox rating: 40HP</li>
+			<li>Cutting height: 2"-10"</li>
+			<li>Deck Material: 12 guage</li>
+			<li>Approximate weight: 470lbs</li>
+		</ul>
+
+		<h2>20FT Utility Trailer</h2>
+
+		<ul>
+			<li>Two 3500lb axles</li>
+			<li>Slide in ramps</li>
+			<li>New Tires</li>
+			<li>Set back jack</li>
+			<li>Trailer brakes</li>
+		</ul>
+
+	</div><!--right-->
+
+	<div class="clear"></div>
+
+</div><!--info-->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
