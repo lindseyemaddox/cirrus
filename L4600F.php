442dba6137a0daabb6934040bac35905f790e6c1
Lindsey Maddox <lindseyemaddox@gmail.com>
2014-12-01 11:33:02 -0600

    straggler...

---
+++ L4600F.php
@@ -0,0 +1,156 @@
+<!doctype html>
+
+<!--[if IE 9 ]><html lang="en" class="ie9"><![endif]-->
+<!--[if lt IE 9 ]><html lang="en" class="ie8"><![endif]-->
+<!--[if lt IE 8 ]><html lang="en" class="ie7"><![endif]-->
+<!--[if (gt IE 7)|!(IE)]><!--><html lang="en" class="no-js"><!--<![endif]-->
+
+<head>
+	<meta charset="utf-8">
+	
+	<!-- Stylesheets -->
+	<link rel="stylesheet" href="/_styles/css/common.css" media="screen">
+
+</head>
+
+<div class="info">
+
+	<div class="header">
+		
+		<h1><img src="/_images/logo-kubota-grey.png" alt="Kubota logo grey"></h1>
+
+		<h2>L4600F Package Deal</h2>
+
+		<div class="clear"></div>
+
+	</div><!--header-->
+	
+	<div class="left">
+		
+		<img src="/_images/Kubota-L4600F-Tractor-Package.jpg">
+
+		<h3>Engine</h3>
+
+		<ul>
+			<li>Type<span>Indirect injection, vertical, water-cooled 4-cycle diesel</span></li>
+			<li>Engine gross power<span>46.3HP, 34.5kW @ 2600rpm</span></li>
+			<li>Engine net power<span>43.8HP, 32.7kW @ 2600rpm</span></li>
+			<li>PTO power<span>38.3HP, 28.6kW</span></li>
+			<li>No. of cylinders<span>4</span></li>
+			<li>Bore & stroke<span>3.4 x 3.6in, 87 x 92.4mm</span></li>
+			<li>Total displacement<span>134.1cu.in., 2,197l</span></li>
+			<li>Rated speed<span>2600rpm</span></li>
+			<li>Alternator<span>12V, 40A</span></li>
+			<li>Fuel tank capacity<span>12.7 US gal, 48l</span></li>
+		</ul>
+
+		<h3>Power Take-Off (PTO)</h3>
+
+		<ul>
+			<li>Rear PTO<span>540rpm</span></li>
+			<li>Type<span>Live-independent hydraulic PTO with PTO brake, wet clutch</span></li>
+		</ul>
+
+		<h3>Hydraulic</h3>
+
+		<ul>
+			<li>Lift control type<span>Position control</span></li>
+			<li>Pump capacity (main)<span>7.8gpm, 29.4l/min</span></li>
+			<li>Power steering pump<span>17.9l/min, 4.7gpm</span></li>
+			<li>3-point hitch<span>Category 1</span></li>
+			<li>lift capacity at lift points<span>2870lbs, 1300kg</span></li>
+			<li>lift capacity 24in behind lift points<span>2320lbs, 1053kg</span></li>
+		</ul>
+
+		<h3>Travelling System</h3>
+
+		<ul>
+			<li>Transmission<span>Constant mesh with synchronized shuttle, 8 forward and 8 reverse</span></li>
+			<li>Steering<span>Hydrostatic power steering</span></li>
+			<li>Brake<span>mechanical, wet disc type</span></li>
+			<li>Clutch<span>dry type single stage</span></li>
+		</ul>
+
+		<h3>Tire Size</h3>
+
+		<ul>
+			<li>AG Standard<span>7.5L - 15 front, 14.9 - 24 rear</span></li>
+			<li>TURF Option<span>27 x 8.50 - 15 front, 44 x 18 - 20 rear</span></li>
+		</ul>
+
+		<h3>Travelling speed with standard AG tires</h3>
+
+		<ul>
+			<li>Forward<span>1.4mph, 2.2km.h - 14.9mph, 24.0km/h</span></li>
+			<li>Reverse<span>1.3mph, 2.1km/h - 14.3mph, 23.0km/h</span></li>
+		</ul>
+
+		<h3>Dimensions</h3>
+
+		<ul>
+			<li>Overall length<span>122.8in, 3120mm</span></li>
+			<li>Overall height<span>91.7in, 2330mm</span></li>
+			<li>Overall width<span>62.4in, 1585mm</span></li>
+			<li>Wheelbase<span>72.8in, 1850mm</span></li>
+			<li>Minimum ground clearance<span>15.2in, 385mm</span></li>
+			<li>Tread<span>front: 50.4, 54.3, 58.3, 62.2in, rear: 46.5, 47.2, 51.2, 57.1, 60.8in</span></li>
+			<li>Minimum thruing radius<span>8.9feet, 2.7m</span></li>
+		</ul>
+		
+		<h3>Weight</h3>
+		
+		<ul>
+			<li>with ROPS<span>3109lbs, 1410kg</span></li>
+		</ul>
+
+	</div><!--left-->
+
+	<div class="right">
+			
+		<img src="/_images/no-image.jpg">
+
+		<h2>6FT Rotary Cutter</h2>
+
+		<ul>
+			<li>Gearbox rating: 40HP</li>
+			<li>Cutting height: 2"-10"</li>
+			<li>Deck Material: 11 guage</li>
+			<li>Approximate weight: 672lbs</li>
+		</ul>
+
+		<h2>20FT Utility Trailer</h2>
+
+		<ul>
+			<li>Two 3500lb axles</li>
+			<li>Slide in ramps</li>
+			<li>New Tires</li>
+			<li>Set back jack</li>
+			<li>Trailer brakes</li>
+		</ul>
+
+	</div><!--right-->
+
+	<div class="clear"></div>
+
+</div><!--info-->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
